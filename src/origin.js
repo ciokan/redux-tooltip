a75fee600dfe97f421044ed986fccf7f85a50081
Romeo Mihalcea <romeo.mihalcea@gmail.com>
2019-05-21 12:59:11 +0300

    Update origin.js

--- src/origin.js
+++ src/origin.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import PropTypes from 'react-proptypes';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import blacklist from 'blacklist';
 import { show, hide, delay } from './actions';
