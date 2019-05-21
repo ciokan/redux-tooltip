2fb4ca7a8ea8fc5b095df8631e2563e8651b2985
Romeo Mihalcea <romeo.mihalcea@gmail.com>
2019-05-21 12:58:50 +0300

    Update tooltip.js

--- src/tooltip.js
+++ src/tooltip.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import PropTypes from 'react-proptypes';
+import PropTypes from 'prop-types';
 import ReactDOM from 'react-dom';
 import { connect } from 'react-redux';
 import { sanitize } from 'dompurify';
